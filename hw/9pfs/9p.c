2b68be790983affebc08946b25350d2ddc4a353f
Costin Lupu <costin.lupu@cs.pub.ro>
2022-02-10 20:01:36 +0200

    cloning: hw/9pfs/9p.c: Reduce qpf table size
    Signed-off-by: Costin Lupu <costin.lupu@cs.pub.ro>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -800,7 +800,7 @@ static void qpp_table_init(struct qht *ht)
 
 static void qpf_table_init(struct qht *ht)
 {
-    qht_init(ht, qpf_cmp_func, 1 << 16, QHT_MODE_AUTO_RESIZE);
+    qht_init(ht, qpf_cmp_func, 1, QHT_MODE_AUTO_RESIZE);
 }
 
 /*
